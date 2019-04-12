723ab7da2245b621019d7a6c3efc404d63f6e47d
じゅえり <supirs102@gmail.com>
2019-04-12 20:00:22 +0900

    データベース名を変更

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@
 
 // ** MySQL 設定 - この情報はホスティング先から入手してください。 ** //
 /** WordPress のためのデータベース名 */
-define('DB_NAME', 'wpmb');
+define('DB_NAME', 'prism_blue');
 
 /** MySQL データベースのユーザー名 */
 define('DB_USER', 'root');
