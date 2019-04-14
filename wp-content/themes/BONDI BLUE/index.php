abdd2c8229b3706f7db396ea90e39903977ffa30
じゅえり <supirs102@gmail.com>
2019-04-15 05:42:19 +0900

    TOPページ枠組み初期作成続き

--- wp-content/themes/BONDI BLUE/index.php
+++ wp-content/themes/BONDI BLUE/index.php
@@ -41,7 +41,36 @@
                 <li><a href="">CONTACT</a></li>
             </ul>
         </div><!-- #menu -->
-        <div id="worklist"></div><!-- #worklist -->
+        <div id="worklist">
+            <ul id="worklist1">
+                <li><a href="">真夏のサイダー</a></li>
+                <li><a href="">水しぶき</a></li>
+                <li><a href="">夏の海が光る。</a></li>
+                <li><a href="">花あそび</a></li>
+            </ul>
+            <ul id="worklist2">
+                <li><a href="">梅雨の夕暮れ</a></li>
+                <li><a href="">紫陽花の水やり</a></li>
+                <li><a href="">夜明け前の海。</a></li>
+                <li><a href="">夏雲</a></li>
+            </ul>
+            <ul id="worklist3">
+                <li><a href="">旅行中に見つけた向日葵畑</a></li>
+                <li><a href="">夏の匂い。</a></li>
+                <li><a href="">海での時間</a></li>
+            </ul>
+            <ul id="worklist4">
+                <li><a href="">夏の日差し</a></li>
+                <li><a href="">光の模様</a></li>
+                <li><a href="">初夏</a></li>
+                <li><a href="">夏の窓枠</a></li>
+            </ul>
+            <ul id="worklist5">
+                <li><a href="">夏の午後</a></li>
+                <li><a href="">プールの水しぶき</a></li>
+                <li><a href="">夏の音</a></li>
+            </ul>
+        </div><!-- #worklist -->
         <footer>
             <div id="footerleft"><p>2019 Image by iwakura shiori</p></div><!-- #footerleft -->
             <div id="footerright">
