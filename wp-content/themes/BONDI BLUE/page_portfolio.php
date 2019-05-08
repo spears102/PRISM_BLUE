eb1f259add781b664702b77b8b02e836919f1840
SSM <cvwwvc102@gmail.com>
2019-05-08 14:40:38 +0900

    Portofolio.phpの問題を修正　要確認

--- wp-content/themes/BONDI BLUE/page_portfolio.php
+++ wp-content/themes/BONDI BLUE/page_portfolio.php
@@ -4,14 +4,31 @@ Template Name: Page_portfolio
 */
 ?>
 <?php get_header(); ?>
-        <div id="portfoliolist"></div><!-- #portfoliolist -->
-        <footer>
-            <div id="footerleft"><p>2019 Image by iwakura shiori</p></div><!-- #footerleft -->
-            <div id="footerright">
-                <a href=""><img src="<?php bloginfo('template_directory'); ?>/img/prism-icon-sample.jpg"></a>
-            </div><!-- #footerright -->
-        </footer>
-        <div class="clear"></div><!-- .clear -->
-    </div><!-- #wrapper -->
-  </body>
-</html>
+        <div id="portfolio">
+            <ul>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample007.jpg">真夏のサイダー</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample012.jpg">夏の匂い。</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample013.jpg">初夏</a></li>
+            </ul>
+            <ul>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample014.jpg">旅行中に見つけた向日葵畑</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample015.jpg">夜明け前の海。</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample016.jpg">光の模様</a></li>
+            </ul>
+            <ul>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample017.jpg">夏の午後</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample013.jpg">光の匂い</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample007.jpg">桜と二人</a></li>
+            </ul>
+            <ul>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample007.jpg">真夏のサイダー</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample012.jpg">夏の匂い。</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample013.jpg">初夏</a></li>
+            </ul>
+            <ul>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample014.jpg">旅行中に見つけた向日葵畑</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample015.jpg">夜明け前の海。</a></li>
+                <li><a href=""><img src="<?php bloginfo('template_directory'); ?>/img/bondiblue-back-sample016.jpg">光の模様</a></li>
+            </ul>
+        </div><!-- #portfolio -->
+<?php get_footer(); ?>
