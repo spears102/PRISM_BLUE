4f092345c5abdfd44944d930d418736301dc3433
じゅえり <supirs102@gmail.com>
2019-05-10 00:14:49 +0900

    TOPページ枠組み初期作成続き

--- wp-content/themes/BONDI BLUE/index.php
+++ wp-content/themes/BONDI BLUE/index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-        <div id="worklist">
+        <div id="home_typea">
             <ul>
                 <li><a href="">真夏のサイダー</a></li>
                 <li><a href="">水しぶき</a></li>
@@ -28,5 +28,80 @@
                 <li><a href="">プールの水しぶき</a></li>
                 <li><a href="">夏の音</a></li>
             </ul>
-        </div><!-- #worklist -->
+        </div><!-- #home_typea -->
+        <div id="home_typeb">
+            <ul>
+                <li><a href="">真夏のサイダー</a></li>
+                <li><a href="">水しぶき</a></li>
+                <li><a href="">夏の海が光る。</a></li>
+                <li><a href="">花あそび</a></li>
+                <li><a href="">梅雨の夕暮れ</a></li>
+                <li><a href="">紫陽花の水やり</a></li>
+                <li><a href="">夜明け前の海。</a></li>
+                <li><a href="">夏雲</a></li>
+            </ul>
+            <ul>
+                <li><a href="">旅行中に見つけた向日葵畑</a></li>
+                <li><a href="">夏の匂い。</a></li>
+                <li><a href="">海での時間</a></li>
+                <li><a href="">夏の日差し</li>
+                <li><a href="">光の模様</li>
+                <li><a href="">初夏</li>
+                <li><a href="">夏の窓枠</li>
+            </ul>
+            <ul>
+                <li><a href="">夏の午後</a></li>
+                <li><a href="">プールの水しぶき</a></li>
+                <li><a href="">夏の音真夏のサイダー</a></li>
+                <li><a href="">水しぶき</a></li>
+                <li><a href="">夏の海が光る。</a></li>
+                <li><a href="">花あそび</a></li>
+                <li><a href="">梅雨の夕暮れ</a></li>
+                <li><a href="">紫陽花の水やり</a></li>
+            </ul>
+        </div><!-- #home_typeb -->
+        <div id="home_typec">
+            <ul>
+                <li><a href="">真夏のサイダー</a></li>
+                <li><a href="">水しぶき</a></li>
+                <li><a href="">夏の海が光る。</a></li>
+                <li><a href="">花あそび</a></li>
+                <li><a href="">梅雨の夕暮れ</a></li>
+                <li><a href="">紫陽花の水やり</a></li>
+                <li><a href="">夜明け前の海。</a></li>
+                <li><a href="">夏雲</a></li>
+                <li><a href="">プールの水しぶき</a></li>
+                <li><a href="">旅行中に見つけた向日葵畑</a></li>
+                <li><a href="">夏の匂い。</a></li>
+                <li><a href="">海での時間</a></li>
+                <li><a href="">夏の日差し</a></li>
+                <li><a href="">光の模様</a></li>
+                <li><a href="">初夏</a></li>
+                <li><a href="">夏の窓枠</a></li>
+                <li><a href="">夏の午後</a></li>
+                <li><a href="">夏の音</a></li>
+                </ul>
+        </div><!-- #home_typec -->
+        <div id="home_typed">
+            <ul>
+                <li><a href="">真夏のサイダー</a></li>
+                <li><a href="">水しぶき</a></li>
+                <li><a href="">夏の海が光る。</a></li>
+                <li><a href="">花あそび</a></li>
+                <li><a href="">梅雨の夕暮れ</a></li>
+                <li><a href="">紫陽花の水やり</a></li>
+                <li><a href="">夜明け前の海。</a></li>
+                <li><a href="">夏雲</a></li>
+                <li><a href="">プールの水しぶき</a></li>
+                <li><a href="">旅行中に見つけた向日葵畑</a></li>
+                <li><a href="">夏の匂い。</a></li>
+                <li><a href="">海での時間</a></li>
+                <li><a href="">夏の日差し</a></li>
+                <li><a href="">光の模様</a></li>
+                <li><a href="">初夏</a></li>
+                <li><a href="">夏の窓枠</a></li>
+                <li><a href="">夏の午後</a></li>
+                <li><a href="">夏の音</a></li>
+                </ul>
+        </div><!-- #home_typed -->
 <?php get_footer(); ?>
